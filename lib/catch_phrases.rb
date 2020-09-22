8fcd7c7262bfddb9c6fd21737b9b125e8f80733c
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-22 20:12:29 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 def mario
-  puts "It's-a me, Mario!"
+  phrase =  "It's-a me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
@@ -7,3 +7,6 @@ end
 def toadstool
   puts status
 end
+
+def link
+  puts 
